dca7ce5d5042cc8a477cbe748e72c6f24e9c132a
Djordje1998 <djordjen7@gmail.com>
2021-03-10 00:06:10 +0100

    Dodate metode toString, hashCode i equals u klase Artikal i KucnaHemija.

--- src/domen/Artikal.java
+++ src/domen/Artikal.java
@@ -31,5 +31,46 @@ public class Artikal {
 	public void setKolicija(int kolicija) {
 		this.kolicija = kolicija;
 	}
+	@Override
+	public String toString() {
+		return "Artikal [naziv=" + naziv + ", sifra=" + sifra + ", opis=" + opis + ", kolicija=" + kolicija + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + kolicija;
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result + ((opis == null) ? 0 : opis.hashCode());
+		result = prime * result + sifra;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Artikal other = (Artikal) obj;
+		if (kolicija != other.kolicija)
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (opis == null) {
+			if (other.opis != null)
+				return false;
+		} else if (!opis.equals(other.opis))
+			return false;
+		if (sifra != other.sifra)
+			return false;
+		return true;
+	}
+	
+	
 
 }
