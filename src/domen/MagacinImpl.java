f32d7c1a0c3ba27d74c9fd8d5a1b58876ce40da1
Djordje1998 <djordjen7@gmail.com>
2021-03-10 01:39:33 +0100

    Jednostavna implementacije klase MagacinImpl

--- src/domen/MagacinImpl.java
+++ src/domen/MagacinImpl.java
@@ -1,24 +1,50 @@
 package domen;
 
+import java.util.LinkedList;
+
 import interfejs.MagacinInterface;
 
 public class MagacinImpl implements MagacinInterface {
 
+	private LinkedList<Artikal> artikli = new LinkedList<Artikal>();
+	
 	@Override
 	public void dodajArtikal(Artikal artikal) {
-		// TODO Auto-generated method stub
-
+		if(artikli.contains(artikal)) {
+			for (Artikal art : artikli) {
+				if(art.getSifra()==artikal.getSifra()) {
+					artikal.setKolicina(artikal.getKolicina()+1);
+					return;
+				}
+			}
+		}
+		
 	}
-
+	
 	@Override
 	public void izbaciArtikal(Artikal artikal) {
-		// TODO Auto-generated method stub
+		if(artikli.contains(artikal)) {
+			for (Artikal art : artikli) {
+				if(art.getSifra()==artikal.getSifra()) {
+					if(artikal.getKolicina()>0) {
+						artikal.setKolicina(artikal.getKolicina()-1);
+					}
+				}
+			}
+		}
 
 	}
+	
 
 	@Override
 	public Artikal pronadjiArtikal(int sifra) {
-		// TODO Auto-generated method stub
+		if(sifra>0) {
+			for (Artikal artikal : artikli) {
+				if(artikal.getSifra()==sifra) {
+					return artikal;
+				}
+			}
+		}
 		return null;
 	}
 
